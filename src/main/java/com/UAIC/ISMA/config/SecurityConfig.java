7d3b59a09a58e9ae3b6cb59b22c4a720db4ffa33
unknown <NatyStoik>
2025-04-13 21:58:12 +0300

    Logica paginii de register

--- src/main/java/com/UAIC/ISMA/config/SecurityConfig.java
+++ src/main/java/com/UAIC/ISMA/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        //.requestMatchers("/api/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasAnyRole("STUDENT", "COORDONATOR")
                         .anyRequest().authenticated()
