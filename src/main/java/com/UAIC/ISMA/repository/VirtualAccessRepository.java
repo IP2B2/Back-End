82e15a1c96e9749b2457f4b1ed5fd51f12a8c146
KlynLeo <calinleonte27@gmail.com>
2025-05-04 19:15:25 +0300

    fixed failing tests

--- src/main/java/com/UAIC/ISMA/repository/VirtualAccessRepository.java
+++ src/main/java/com/UAIC/ISMA/repository/VirtualAccessRepository.java
@@ -4,6 +4,11 @@ import com.UAIC.ISMA.dao.VirtualAccess;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VirtualAccessRepository extends JpaRepository<VirtualAccess, Long> {
+    boolean existsByAccessRequest_Id(Long accessRequestId);
+    Optional<VirtualAccess> findByAccessRequest_Id(Long accessRequestId);
+
 }
