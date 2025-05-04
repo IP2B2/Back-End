4228554d285e5260e89a9eee3fa8a828a53b135d
radiogardvs <radiogardvs@gmail.com>
2025-05-04 22:51:29 +0300

    test

--- src/main/java/com/UAIC/ISMA/IsmaApplication.java
+++ src/main/java/com/UAIC/ISMA/IsmaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IsmaApplication {
 
-/// test222
+/// test222333
     public static void main(String[] args) {
         SpringApplication.run(IsmaApplication.class, args);
     }
