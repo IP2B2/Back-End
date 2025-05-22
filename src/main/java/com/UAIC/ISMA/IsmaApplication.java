04ef31ffb034b839afbce9c8cfeea76b42f134d1
radiogardvs <radiogardvs@gmail.com>
2025-05-22 21:54:17 +0300

    fix for testing environment

--- src/main/java/com/UAIC/ISMA/IsmaApplication.java
+++ src/main/java/com/UAIC/ISMA/IsmaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IsmaApplication {
+    //BEST APP EVER
     public static void main(String[] args) {
         SpringApplication.run(IsmaApplication.class, args);
     }
