238dde89e2fe43fa7511cde510e0844e65813fd5
KlynLeo <calinleonte27@gmail.com>
2025-05-24 14:29:22 +0300

    endpoint pt a obtine rolurile userului conectat

--- src/main/java/com/UAIC/ISMA/controller/AuthController.java
+++ src/main/java/com/UAIC/ISMA/controller/AuthController.java
@@ -14,10 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -57,6 +54,14 @@ public class AuthController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Something went wrong"));
         }
+    }
+
+    @GetMapping("/roles")
+    public List<String> getCurrentUserRoles(Authentication authentication) {
+        return authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
     }
 
 }
